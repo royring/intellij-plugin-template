d4115c7a3bf33a9fe508b1bc17010a7bfcfddcc5
GitHub Action <action@github.com>
2022-01-18 06:22:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/royring/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.royring.intellijplugintemplate.services
+
+import com.github.royring.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/royring/intellijplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/royring/intellijplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.royring.intellijplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.royring.intellijplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
